5f6c0d006ffd956033e57a91a4c5ba7906e2ad1d
yangdekun <1216200257@qq.com>
2018-04-18 01:25:45 +0800

    2222

--- eureka-hi1/src/main/java/cn/yang/springcloud/eureka_hi1/test2.java
+++ eureka-hi1/src/main/java/cn/yang/springcloud/eureka_hi1/test2.java
@@ -2,15 +2,8 @@ package cn.yang.springcloud.eureka_hi1;
 
 public class test2
 {
-<<<<<<< .mine
     public static void main(String[] args)
     {
         
     }
-=======
-    public static void main(String[] args)
-    {
-        System.out.println("111");
-    }
->>>>>>> .theirs
 }
--- eureka-hi1/src/main/java/cn/yang/springcloud/eureka_hi1/test3.java
+++ eureka-hi1/src/main/java/cn/yang/springcloud/eureka_hi1/test3.java
@@ -2,5 +2,9 @@ package cn.yang.springcloud.eureka_hi1;
 
 public class test3
 {
-    
+    private void syso()
+    {
+        // TODO Auto-generated method stub
+        
+    }
 }
